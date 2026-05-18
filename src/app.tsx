af86072b900b47a14502404a340e8c329d2f648e
Melvin Saúl <msksaul@gmail.com>
2026-05-18 15:31:13 -0600

    initial configuration

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import Header from './components/header'
+
+function App() {
+  return (
+    <div className='max-w-2xl mx-auto p-4 flex flex-col gap-4'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/button.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from 'react'
+
+type ButtonProps = {
+  children: ReactNode
+}
+
+const Button = ({children}: ButtonProps) => {
+  return (
+    <button 
+      className='bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 disabled:opacity-30 disabled:cursor-not-allowed'
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
